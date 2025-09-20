57d51fc1cd19a82a47fc933c8520bdf4f1e5d760
Oscar <oscarodriguez56@gmail.com>
2025-09-20 12:13:39 +0200

    En la maquina de cafe se añade otra llamada a hacer cafe

--- src/Cafe.kt
+++ src/Cafe.kt
@@ -1,1 +1,7 @@
+/**
+ * DataClass que representa los cafes
+ * @property precio el precio de los cafes
+ * @property cantidadAzucar la cantidad de azucar del cafe
+ * @property tipo el tipo de Cafe
+ */
 data class Cafe(var precio: Double, var cantidadAzucar: Int, var tipo: TiposCafe)
--- src/MaquinaCafe.kt
+++ src/MaquinaCafe.kt
@@ -1,26 +1,28 @@
 object MaquinaCafe {
     private var estadoActual: EstadosMaquinas = EstadosMaquinas.Idle
     fun hacerCafe(tipo: Cafe, monedas: Double) {
-        println("Estado ${estadoActual}")
+        println("Estado $estadoActual")
         when (estadoActual) {
             is EstadosMaquinas.Idle -> {
-                if (tipo.precio >= monedas) {
+                if (tipo.precio <= monedas) {
                     println("Empezando a preparar cafe")
                     estadoActual = EstadosMaquinas.preparandoCafe
                     println("Preparando ${tipo.tipo}")
                     estadoActual = EstadosMaquinas.sirviendoCafe(tipo.tipo.name)
+                    hacerCafe(tipo,monedas)
                 } else {
                     println("No tienes suficiente dinero")
                 }
             }
             is EstadosMaquinas.sirviendoCafe -> {
-                println("Sirviendo ${marca}")
+                println("Sirviendo ${(estadoActual as EstadosMaquinas.sirviendoCafe).marca}")
                 if (tipo.precio > monedas) {
                     println("Abonando la vuelta ${tipo.precio-monedas}")
                 }
+                println("Cafe servido")
             }
             is EstadosMaquinas.fallo -> {
-                println("")
+                println("Fallo: ${(estadoActual as EstadosMaquinas.fallo).error}")
             }
             else -> {
                 println("Que mierda haces")
