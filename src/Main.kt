9a2052ff9f65bec7a9e93bdc8f12610cfc136c5b
Oscar Rodriguez <orodriguezcabaleiro@danielcastelao.org>
2025-09-23 13:02:27 +0200

    Volviendo a manejar las cosas del cafe

--- src/Main.kt
+++ src/Main.kt
@@ -1,8 +1,13 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    val cafe = Cafe(2.2,2, TiposCafe.CAPPUCCINO,0)
-    MaquinaCafe.hacerCafe()
-    MaquinaCafe.hacerCafe()
-    MaquinaCafe.hacerCafe()
+    val cafe = Cafe(1.23,5, TiposCafe.CHOCOLATE,5)
+    println("--- Encendiendo maquina ---")
+    MaquinaCafe.hacerCafe(cafe,1.23)
+    println("--- Intentando hacer café de nuevo --")
+    MaquinaCafe.hacerCafe(cafe,0.10)
+    println("--- Limpiando ---")
+    MaquinaCafe.clean()
+    println("--- Preparando cafe otra vez ---")
+    MaquinaCafe.hacerCafe(cafe,4.23)
 }
--- src/MaquinaCafe.kt
+++ src/MaquinaCafe.kt
@@ -1,27 +1,50 @@
 object MaquinaCafe {
     private var estadoActual: EstadosMaquinas = EstadosMaquinas.Idle
-    fun hacerCafe() {
-        println("Estado $estadoActual")
+    private var filtroLimpio = true
+    private var cafesHechos = 0
+    fun hacerCafe(cafe: Cafe, monedas: Double) {
         when (estadoActual) {
             is EstadosMaquinas.Idle -> {
-                println("Empezando a preparar cafe")
-                estadoActual = EstadosMaquinas.preparandoCafe
-                    //hacerCafe(tipo,monedas) /* Esta llamada recursiva es necesaria si quieres
-                                            // quieres que actualice solo el estado */
+                if (monedas>cafe.precio) {
+                    println("Empezando a preparar cafe")
+                    estadoActual = EstadosMaquinas.preparandoCafe
+                    hacerCafe(cafe, monedas) /* Esta llamada recursiva es necesaria si quieres
+                                            // quieres que actualice solo el estado, al menos en la manera de que */
+                }
             }
             is EstadosMaquinas.preparandoCafe -> {
-                println("Preparando cafe")
-                estadoActual = EstadosMaquinas.sirviendoCafe("catppuccino")
-                //hacerCafe(tipo,monedas)
+                if (cafe.canitdadIngredientes<1 && !filtroLimpio) {
+                    estadoActual = EstadosMaquinas.fallo("No tenemos el material necesario para preparar su producto, y el filtro esta sucio")
+                } else if (cafe.canitdadIngredientes<1) {
+                    estadoActual = EstadosMaquinas.fallo("No tenemos el material necesario para preparar su producto")
+                }
+                else if (!filtroLimpio) {
+                    estadoActual = EstadosMaquinas.fallo("Filtro sucio")
+                } else {
+                    println("Preparando cafe")
+                    estadoActual = EstadosMaquinas.sirviendoCafe("catppuccino")
+                }
+                hacerCafe(cafe,monedas)
             }
             is EstadosMaquinas.sirviendoCafe -> {
                 println("Sirviendo ${(estadoActual as EstadosMaquinas.sirviendoCafe).marca}")
                 println("Cafe servido")
                 estadoActual = EstadosMaquinas.Idle
+                if (monedas>cafe.precio) {
+                    println("Le corresponden ${String.format("%.2f",monedas-cafe.precio)}€ de vuelta")
+                }
+                if (cafesHechos>10) {
+                    filtroLimpio = false
+                }
+                cafesHechos += 1
             }
             is EstadosMaquinas.fallo -> {
                 println("Fallo: ${(estadoActual as EstadosMaquinas.fallo).error}")
             }
         }
+    }
+    fun clean() {
+        filtroLimpio = true
+        estadoActual = EstadosMaquinas.Idle
     }
 }
